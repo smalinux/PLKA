f4cd97b2d7b7fba60fee74ae66fea60ae9654e6b
Sohaib Mohammed <sohaibfacebook396@gmail.com>
2020-04-27 18:26:17 +0200

    Log to 2.3.3 and break decision

---
+++ log.h
@@ -0,0 +1,13 @@
+Skipped
+=======================
+- About the Author
+- Acknowledgments
+- Introduction and Overview (33: 65)
+- 
+
+2.2 Process Life Cycle ✔ ✔ ✔
+	2.2.1 Preemptive Multitasking ✘ ✘
+2.3 Process Representation ✔ ✔ ✔
+	2.3.1 Process Types  ✔ ✔ ✔
+	2.3.2 Namespaces  ✘ ✘
+	2.3.3 Process Identification Numbers 
--- main.c
+++ main.c
@@ -1,1 +1,39 @@
-hi
+processes types:
+	1- Real-time processes (ex: RTOS, RTLinux, Xenomai, RATI) 
+	2- Not Real-time (ex: Linux)
+
+Linux supports different scheduling classes
+	fair scheduling, realtime scheduling, so on...
+
+
+
+> all processes in a process table == 
+
+
+
+
++--------+
+|syscalls|
++--------+
+
+
+==============================================================================
+
+struct task_struct
+/*
+Each process in the system uses certain amount of different resources like files,
+	CPU time, memory and so on.
+Such resources are not infinite and each process and we should have an instrument to 
+	manage it. Sometimes it is useful to know current limits for a certain resource or 
+	to change it's value.
+
+task_struct->rlimit
+*/
+struct rlimit // resources limit
+	cat /proc/1816/limits
+	best resource: https://github.com/smalinux/linux-insides/blob/master/SysCall/linux-syscall-6.md
+	My linux books
+/*
+Google: linux kernel Namespaces 
+*/
+struct nsproxy
